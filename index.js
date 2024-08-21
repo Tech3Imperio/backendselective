b5c0fd852239ba9e1618312148922c9f87487e7b
Tech3 <Tech3@imperiorailing.com>
2024-08-21 11:48:35 +0530

    chnages

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from the React frontend
+    origin: process.env.BASE_URL || "http://localhost:3000", // Dynamically set origin
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type",
   })
